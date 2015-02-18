569863920d48a89f8fb8a07419c4863c03f71a3e
sidgopinath <sdg23@duke.edu>
2015-02-18 16:17:15 -0500

    Added rough draft of skeleton methods in Controller and Main.

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,5 +1,29 @@
 package controller;
 
-public class Main {
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+	private SLogoController sLogoController;
+	public static final int HEIGHT = 600;
+	public static final int WIDTH = 600;
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("SLogoController");
+		sLogoController = new SLogoController(HEIGHT, WIDTH, stage);
+		stage.setScene(sLogoController.getScene());
+		stage.setResizable(false);
+		stage.show();
+		Timeline animationTimeline = new Timeline(); //could be moved to controller?
+		int frameRate = 60;
+		sLogoController.setFrameRate(frameRate);
+		sLogoController.manageTimeline(animationTimeline, frameRate); //could be moved to controller
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}	
 }

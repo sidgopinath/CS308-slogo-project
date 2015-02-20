70ae3a7f77bccc98184af71da118286ee2627f15
Callie Mao <callie.mao@gmail.com>
2015-02-20 00:12:32 -0500

    Added on base code for main, view, and controller

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,5 +1,19 @@
 package controller;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+	SLogoController myController;
+	
+	@Override
+	public void start(Stage s) throws Exception {
+		myController = new SLogoController(s);
+
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
--- src/controller/SLogoController.java
+++ src/controller/SLogoController.java
@@ -1,5 +1,20 @@
 package controller;
 
+import javafx.stage.Stage;
+
+import model.Parser;
+import view.SLogoView;
+
+;
+
 public class SLogoController {
+
+	private Parser myParser;
+	private SLogoView myView;
+
+	public SLogoController(Stage s) {
+		myParser = new Parser();
+		myView = new SLogoView(s);
+	}
 
 }

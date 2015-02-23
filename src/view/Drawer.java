f63de6b42e6c07cdf3a3b37ec7841bad299e064e
mf108 <mf108@duke.edu>
2015-02-23 18:11:45 -0500

    updated drawer

--- src/view/Drawer.java
+++ src/view/Drawer.java
@@ -14,35 +14,32 @@ import model.instructions.Instruction;
 public class Drawer {
     private Color color = Color.BLACK;
 
-       public List<Polyline> draw(Map<Integer, ImageView> turtles, ArrayList<Instruction> instrucions){
-           ArrayList<Polyline> lines= new ArrayList<Polyline>();
-           Iterator<Instruction> it = instrucions.iterator();
-           while(it.hasNext()){
-               Instruction instruction = it.next();
-               ImageView turtle = turtles.get(instruction.turtleId);
-               Polar polar = instruction.polar;
-               double turtleX=turtle.getX();
-               double turtleY=turtle.getY();
-               if(instruction.jump){
-                   
+   public List<Polyline> draw(Map<Integer, ImageView> turtles, ArrayList<Instruction> instrucions){
+       ArrayList<Polyline> lines= new ArrayList<Polyline>();
+       Iterator<Instruction> it = instrucions.iterator();
+       while(it.hasNext()){
+           Instruction instruction = it.next();
+           ImageView turtle = turtles.get(instruction.turtleId);
+           Polar polar = instruction.polar;
+           double turtleX=turtle.getX();
+           double turtleY=turtle.getY();
+           if(polar.distance!=0){
+               double turtleNewX=turtleX+Math.sin(polar.angle)*polar.distance;
+               double turtleNewY=turtleY+Math.cos(polar.angle)*polar.distance;
+               turtle.setX(turtleNewX);
+               turtle.setY(turtleNewY);
+               if(!instruction.penUp){
+                   Polyline polyline = new Polyline();
+                   polyline.setStroke(color);
+                   polyline.getPoints().addAll(new Double[]{turtleX, turtleY,turtleNewX, turtleNewY});
+                   lines.add(polyline);
                }
-               if(polar.distance!=0){
-                   double turtleNewX=turtleX+Math.sin(polar.angle)*polar.distance;
-                   double turtleNewY=turtleY+Math.cos(polar.angle)*polar.distance;
-                   turtle.setX(turtleNewX);
-                   turtle.setY(turtleNewY);
-                   if(!instruction.penUp){
-                       Polyline polyline = new Polyline();
-                       polyline.setStroke(color);
-                       polyline.getPoints().addAll(new Double[]{turtleX, turtleY,turtleNewX, turtleNewY});
-                       lines.add(polyline);
-                   }
-               }else{
-                   turtle.setRotate(turtle.getRotate()+polar.angle);
-               }
+           }else{
+               turtle.setRotate(turtle.getRotate()+polar.angle);
            }
-           return lines;
        }
+       return lines;
+   }
 
     public void changeColor (Color c) {
         color=c;

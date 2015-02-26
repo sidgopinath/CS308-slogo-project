71656272fc1f3b69bc566fc510694aed15751ab9
sidgopinath <sdg23@duke.edu>
2015-02-26 01:12:12 -0500

    Added turtlerequestinstructions and movement instructions.

--- src/model/Parser.java
+++ src/model/Parser.java
@@ -26,7 +26,7 @@ import model.turtle.TurtleCommand;
 public class Parser {
 	private static List<Entry<String, Pattern>> patterns;
 	private static Map<String,String> commandMap;
-	private static final String[] commandTypes = new String[]{"BooleanInstruction","ControlInstruction","MathInstruction","MovementInstruction"};
+	private static final String[] commandTypes = new String[]{"BooleanInstruction","ControlInstruction","MathInstruction","MovementInstruction","TurtleRequestInstruction"};
 	private int furthestDepth;
 	private SLogoView mySLogoView;
 	private ExecutionEnvironment executionParameters;

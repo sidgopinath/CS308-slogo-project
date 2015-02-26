7e4a7732feaab48b05c522cf5b2b28d19b148610
Greg McKeon <grm19@duke.edu>
2015-02-25 23:38:21 -0500

    fixed node

--- src/model/Node.java
+++ src/model/Node.java
@@ -6,15 +6,27 @@ import java.util.List;
 import model.instructions.Instruction;
 
 public class Node{
+		
 		private List<Node> myChildren;
+		private String myValue;
 		private Instruction myInstruction;
-		public Node(Instruction myValue){
-			myInstruction = myValue;
+		public Node(String me){
+			myValue = me;
 			myChildren = new ArrayList<Node>();
+		}
+		public String getValue(){
+			return myValue;
 		}
 	public Node addChild(Node n){
 		myChildren.add(n);
 		return n;
+	}
+	public String[] childrenToStringArray(){
+		String[] output = new String [myChildren.size()];
+		for(int i = 0; i<myChildren.size();i++){
+			output[i] = myChildren.get(i).getValue();
+		}
+		return output;
 	}
 	public List<Node> getChildren(){
 		return myChildren;

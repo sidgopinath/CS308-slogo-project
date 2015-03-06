41569535b2a01d9605c5b22288e3a4905eb0e9c4
mf108 <mf108@duke.edu>
2015-03-05 20:57:30 -0500

    edited SLogoView

--- src/view/SLogoView.java
+++ src/view/SLogoView.java
@@ -62,134 +62,7 @@ public class SLogoView implements Observer {
 		lines.setManaged(false);
 	}
 
-	 public GridPane configureUI() {
-	  GridPane root = new GridPane();
-      setGridPaneConstraints(root);
-    
-      // set initial turtle
-      TurtleView turtle = new TurtleView(0, new Image(Strings.DEFAULT_TURTLE_IMG));
-      myTurtles.put(0, turtle);
-    
-      //TODO: move tabpane to mainview and create a gridpane within tabview
-      mySidebar = new SideBar(myTurtles, myController);
-      setDefaultWorkspace();
-      //root.add(configureTopMenu(), 0, 0, 2, 1);
-      root.add(new CustomizationBar(myController, myTurtles, drawer, myWorkspace,
-              myStage, myDimensions), 0, 0);
-      root.add(configureAddTurtlesButton(), 1, 0);
-      root.add(myWorkspace, 0, 1);
-      root.add(mySidebar, 1, 1, 1, 2);
-      myEditor = new Editor(myController, mySidebar, myDimensions);
-      root.add(myEditor, 0, 2);
-	  root.setGridLinesVisible(true);
-	  return root;
-    }
-
-	// does this do anything?
-	// do we need to return anything
-
-	// do we still need an entire list for this?
-	public void updateWorkspace(List<TurtleCommand> instructionList) {
-		// String returnString = null;
-		/*
-		 * for (TurtleCommand instruction : instructionList) { returnString +=
-		 * updateFromInstruction(instruction) + "\n"; }
-		 */
-		List<Polyline> newlines = drawer.draw(myTurtles, instructionList, mySidebar);
-		lines.getChildren().addAll(newlines);
-
-		// return returnString;
-	}
-
-	// make update from a single command
-	/*
-	 * private String updateFromInstruction(TurtleCommand instruction) { return
-	 * "return value"; }
-	 */
-
-	public Turtle getTurtleInfo(int index) {
-		ImageView temp = myTurtles.get(index);
-		return new Turtle(temp.getX(), temp.getY(), temp.getRotate());
-
-	}
-
-	// this one is not actually used
-	// TODO: what is being passed in and how to update the tableview? may have
-	// to iterate through observablelist
-	public void updateVariables(Map<String, Double> variableUpdates) {
-		Iterator<Entry<String, Double>> it = variableUpdates.entrySet().iterator();
-		while (it.hasNext()) {
-			Entry<String, Double> variable = it.next();
-			String name = variable.getKey();
-			double value = variable.getValue();
-			if (variables.get(name) == null) {
-				// TODO: it should be passed in not as a map but as the actual
-				// variable object in the parameter
-				// or we can just keep the variables object as just a front end
-				// thing for displaying (otherwise both front and back end have
-				// access to it which may not be good)
-				mySidebar.updateVariable(new VariableView(name, value));
-			} else {
-				// variables.get(name).setText(value);
-			}
-		}
-	}
-
-	public double towards(int id, double x, double y) {
-		double angle = Math.toDegrees(Math.atan2(x, y));
-		return setHeading(id, angle, false);
-	}
-
-	// for testing
-	/*
-	 * private void handleKeyInput(KeyEvent e) { KeyCode keyCode = e.getCode();
-	 * if (keyCode == KeyCode.D) { ArrayList<TurtleCommand> instructions = new
-	 * ArrayList<TurtleCommand>(); instructions.add(new TurtleCommand(0, new
-	 * Polar(30, 0), true)); List<Polyline> newlines = drawer.draw(myTurtles,
-	 * instructions, mySidebar); lines.getChildren().addAll(newlines); } else if
-	 * (keyCode == KeyCode.W) { ArrayList<TurtleCommand> instructions = new
-	 * ArrayList<TurtleCommand>(); instructions.add(new TurtleCommand(0, new
-	 * Polar(0, 15), false)); List<Polyline> newlines = drawer.draw(myTurtles,
-	 * instructions); lines.getChildren().addAll(newlines); } else if (keyCode
-	 * == KeyCode.E) { System.out.print(towards(0, -10, 10)); } else if (keyCode
-	 * == KeyCode.Q) { System.out.print(clearScreen(0)); } else if (keyCode ==
-	 * KeyCode.A) { System.out.print(myTurtles.get(0).getTranslateX() + "," +
-	 * myTurtles.get(0).getTranslateY()); } }
-	 */
-
-
-	// TODO: make into a tab pane
-//	private Text configureTopRow() {
-//		Text title = new Text("SLogo");
-//		title.setFont(new Font(30));
-//		title.setTextAlignment(TextAlignment.CENTER); // why does this not work
-//		return title;
-//		
-//	}
 	private void setGridPaneConstraints(GridPane root) {
-		
-		//TODO: move menu bar to MainView
-		// menu bar
-	/*	RowConstraints row0 = new RowConstraints();
-		row0.setPercentHeight(2);
-		// tabs
-		RowConstraints row1 = new RowConstraints();
-		row1.setPercentHeight(7);
-		// customization bar
-		RowConstraints row2 = new RowConstraints();
-		row2.setPercentHeight(4);
-		// workspace
-		RowConstraints row3 = new RowConstraints();
-		row3.setPercentHeight(67);
-		// editor
-		RowConstraints row4 = new RowConstraints();
-		row4.setPercentHeight(20);
-
-		root.getRowConstraints().add(row0);
-		root.getRowConstraints().add(row1);
-		root.getRowConstraints().add(row2);
-		root.getRowConstraints().add(row3);
-		root.getRowConstraints().add(row4);*/
 		
 		RowConstraints row0 = new RowConstraints();
 		row0.setPercentHeight(4);
@@ -214,9 +87,103 @@ public class SLogoView implements Observer {
 		drawer = new Drawer(myWorkspace.getGridWidth(), myWorkspace.getGridHeight());
 	}
 
+
+    private SLogoController createNewController(SLogoView view) {
+        return new SLogoController(view);
+    }
+
+    private Button configureAddTurtlesButton() {
+        // Add turtle button
+        Button newTurtleButton = new Button("Add a turtle");
+        newTurtleButton.setStyle("-fx-base: #b6e7c9;");
+        newTurtleButton.setAlignment(Pos.CENTER_RIGHT);
+        newTurtleButton.setOnAction(e -> myWorkspace.addTurtle());
+        GridPane.setHalignment(newTurtleButton, HPos.CENTER);
+        return newTurtleButton;
+    }
+
+    private void updateCommand(String s) {
+        mySidebar.updateCommand(s);
+    }
 	// ---------
 
 	// methods for the backend to call. TODO: organize better
+    public GridPane configureUI() {
+        GridPane root = new GridPane();
+        setGridPaneConstraints(root);
+      
+        // set initial turtle
+        TurtleView turtle = new TurtleView(0, new Image(Strings.DEFAULT_TURTLE_IMG));
+        myTurtles.put(0, turtle);
+      
+        //TODO: move tabpane to mainview and create a gridpane within tabview
+        mySidebar = new SideBar(myTurtles, myController);
+        setDefaultWorkspace();
+        //root.add(configureTopMenu(), 0, 0, 2, 1);
+        root.add(new CustomizationBar(myController, myTurtles, drawer, myWorkspace, myStage, myDimensions), 0, 0);
+        root.add(configureAddTurtlesButton(), 1, 0);
+        root.add(myWorkspace, 0, 1);
+        root.add(mySidebar, 1, 1, 1, 2);
+        myEditor = new Editor(myController, mySidebar, myDimensions);
+        root.add(myEditor, 0, 2);
+        root.setGridLinesVisible(true);
+        return root;
+    }
+
+    // does this do anything?
+    // do we need to return anything
+
+    // do we still need an entire list for this?
+    public void updateWorkspace(List<TurtleCommand> instructionList) {
+        // String returnString = null;
+        /*
+         * for (TurtleCommand instruction : instructionList) { returnString +=
+         * updateFromInstruction(instruction) + "\n"; }
+         */
+        List<Polyline> newlines = drawer.draw(myTurtles, instructionList, mySidebar);
+        lines.getChildren().addAll(newlines);
+
+        // return returnString;
+    }
+
+    // make update from a single command
+    /*
+     * private String updateFromInstruction(TurtleCommand instruction) { return
+     * "return value"; }
+     */
+
+    public Turtle getTurtleInfo(int index) {
+        ImageView temp = myTurtles.get(index);
+        return new Turtle(temp.getX(), temp.getY(), temp.getRotate());
+
+    }
+
+    // this one is not actually used
+    // TODO: what is being passed in and how to update the tableview? may have
+    // to iterate through observablelist
+    public void updateVariables(Map<String, Double> variableUpdates) {
+        Iterator<Entry<String, Double>> it = variableUpdates.entrySet().iterator();
+        while (it.hasNext()) {
+            Entry<String, Double> variable = it.next();
+            String name = variable.getKey();
+            double value = variable.getValue();
+            if (variables.get(name) == null) {
+                // TODO: it should be passed in not as a map but as the actual
+                // variable object in the parameter
+                // or we can just keep the variables object as just a front end
+                // thing for displaying (otherwise both front and back end have
+                // access to it which may not be good)
+                mySidebar.updateVariable(new VariableView(name, value));
+            } else {
+                // variables.get(name).setText(value);
+            }
+        }
+    }
+
+    public double towards(int id, double x, double y) {
+        double angle = Math.toDegrees(Math.atan2(x, y));
+        return setHeading(id, angle, false);
+    }
 
 	public double setXY(int id, double x, double y) {
 		double xy = myTurtles.get(id).setXY(x, y);
@@ -302,10 +269,7 @@ public class SLogoView implements Observer {
 		lines.getChildren().clear();
 		myTurtles.get(id).setAbsoluteHeading(0);
 		double dist = myTurtles.get(id).setXY(0, 0);
-		mySidebar.updateTurtleProperties(id); // dist is used twice so that
-												// turtle properties can be
-												// updated before returning the
-												// value
+		mySidebar.updateTurtleProperties(id);
 		return dist;
 	}
 
@@ -320,29 +284,5 @@ public class SLogoView implements Observer {
 		for (String s : env.getUserCommandMap().keySet()) {
 			updateCommand(s);
 		}
-	}
-
-	public SLogoController createNewController(SLogoView view) {
-		return new SLogoController(view);
-	}
-
-	private Button configureAddTurtlesButton() {
-		// Add turtle button
-		Button newTurtleButton = new Button("Add a turtle");
-		newTurtleButton.setStyle("-fx-base: #b6e7c9;");
-		newTurtleButton.setAlignment(Pos.CENTER_RIGHT);
-		newTurtleButton.setOnAction(e -> myWorkspace.addTurtle());
-		GridPane.setHalignment(newTurtleButton, HPos.CENTER);
-		/*
-		 * TextField textbox = new TextField("" + turtleList.size());
-		 * textbox.setEditable(false);
-		 * textbox.setPrefWidth(dimensions.getWidth()/100);
-		 */
-		// getChildren().addAll(newTurtleButton);
-		return newTurtleButton;
-	}
-
-	private void updateCommand(String s) {
-		mySidebar.updateCommand(s);
 	}
 }
